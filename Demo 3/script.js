c59c6aaca92c94fd25054db59758f0eaab2f6730
akb364 <61127759+akb364@users.noreply.github.com>
2020-11-20 15:04:56 -0700

    Add files via upload

--- Demo 3/script.js
+++ Demo 3/script.js
@@ -1,26 +1,23 @@
-
 // Attempting to create the sign in, attached to a div in the html
 var signIn = new Vue({
-  el: '#app',
-  data: {
-    username: 'Enter Username',
-    input_username: '',
-    password: 'Enter Password',
-    input_password: '',
-    loggedIn: false,
+    el: '#app',
+    data: {
+        username: 'Enter Username',
+        input_username: '',
+        password: 'Enter Password',
+        input_password: '',
+        loggedIn: false,
 
-  },
-  methods: {
-    compareLogIn: function (){
+    },
+    methods: {
+        compareLogIn: function (){
+            if(this.input_username == "Capstone" && this.input_password == "Password"){
+                this.loggedIn = true;
+            }
+            // GOTTA ADD SOMETHING THAT DISPLAYS LIKE SUCCESS OR FAIL
+            else{
 
-      if(this.input_username === "Capstone" && this.input_password === "Password"){
-        this.loggedIn = true;
-      }
-      else{
-        this.loggedIn = false;
-      }
-      // GOTTA ADD SOMETHING THAT DISPLAYS LIKE SUCCESS OR FAIL
-
+            }
+        }
     }
-  }
 })
