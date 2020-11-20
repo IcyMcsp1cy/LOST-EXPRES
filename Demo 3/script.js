0096d565f6ab53743ee70828dc12c9b0ab2a9936
Brooke Caldwell <bec243@nau.edu>
2020-11-20 13:21:32 -0700

    Adding some stuff? maybe its right?

---
+++ Demo 3/script.js
@@ -0,0 +1,13 @@
+
+// Attempting to create the sign in, attached to a div in the html
+var signIn = new Vue({
+  el: '#app',
+  data: {
+    username: 'Username',
+    input_username: '',
+    password: 'Password',
+    input_password: '',
+    loggedIn: true,
+    
+  }
+})
