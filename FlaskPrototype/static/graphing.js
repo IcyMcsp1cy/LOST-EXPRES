7a089b45e6a4d2ad310856eb96d0867ad6cf42f7
Ian McIlrath <yenmcilrath@gmail.com>
2021-02-15 01:39:37 -0700

    client graphing

--- FlaskPrototype/static/graphing.js
+++ FlaskPrototype/static/graphing.js
@@ -1,29 +1,36 @@
 
-if(!window.dash_clientside) {
-    window.dash_clientside = {}
-}
-
-window.dash_clientside.graphing = {
-    clickData: function(clickData, table) {
-        if(clickData === undefined) return;
-
-        let pointData = clickData.points[0];
-        let data = JSON.stringify(table[pointData.pointIndex].FILENAME)
-
-        return data;
-    },
-    zoomData: function(value, data) {
-        if(value === undefined){
-            console.log('undef')
-            return;}
-
-        console.log(value)
-
-        return {
-            'data': data,
-            'layout': {
-                'xaxis': {'range': [value[0], value[1]]}
+window.dash_clientside = Object.assign({}, window.dash_clientside, {
+    graphing: {
+        clickData: function(clickData, table) {
+            if(clickData === undefined) return;
+    
+            let pointData = clickData.points[0];
+            let data = JSON.stringify(table[pointData.pointIndex].FILENAME)
+    
+            return data;
+        },
+        zoomfunc: function (value, table) {
+            data = JSON.parse(table)
+            if(figure === undefined || value === undefined){
+                throw 'input undefined'
             }
+            return {
+                'data':[{
+                    'x': Object.values(data['MJD']),
+                    'y': Object.values(data['V']),
+                    'xaxis': 'x',
+                    'yaxis': 'y',
+                    'type': 'scatter',
+                    'mode': 'markers'
+                }],
+                'layout':{
+                    'xaxis': {
+                        'range': [value[0],value[1]]
+                    },
+                    'type': 'scatter'
+                }
+            }
+            
         }
     }
-}
+});
