eba088be2dd79ec0e9e9f71c3d269fda79b79ed4
Ian McIlrath <yenmcilrath@gmail.com>
2021-02-18 20:23:57 -0700

    admin fix

---
+++ FlaskPrototype/flaskServer/config.py
@@ -0,0 +1,5 @@
+
+FLASK_ENV = 'development'
+DEBUG = True
+SECRET_KEY = 'AlrightThenKeepYourSecrets'
+MONGO_URI = 'mongodb+srv://Dev:dev@cluster0.odagz.mongodb.net/SolarExpres?retryWrites=true&w=majority'
